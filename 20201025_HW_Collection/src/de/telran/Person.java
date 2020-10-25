bb6b441f78a77b565adb7012e0e9e3ae8a43f377
ILLIA-13Morn <skviiirskij@hotmail.com>
2020-10-25 19:42:35 +0100

    HW_Collection

---
+++ 20201025_HW_Collection/src/de/telran/Person.java
@@ -0,0 +1,38 @@
+package de.telran;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private Address address;
+
+    public Person(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    public static List<Address> getAddresses(List<Person> persons) {
+        List<Address> list = new LinkedList<>();
+        for (Person p : persons) {
+            list.add(p.address);
+        }
+        return list;
+    }
+}
